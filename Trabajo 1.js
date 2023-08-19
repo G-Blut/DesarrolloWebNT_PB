97a588a1eb793f1ea62e4a1813c86be73137b896
Jaiber Jiménez <jaiber.sevulpeda494@pascualbravo.edu.co>
2023-08-19 17:13:04 -0500

    Operador ternario exitoso

--- Trabajo 1.js
+++ Trabajo 1.js
@@ -1,3 +1,4 @@
+//-------------------Segmento de variables----------------
 let Person1="Dana"
 
 const Superior = "Gerente"
@@ -21,3 +22,41 @@ Saludar_Personal();
 console.log(Superior)
 console.log(Cordial)
 console.log(Person)
+
+
+//-----------------Segmento de condicionales---------------------
+
+//Logical Operator ||
+
+true || false;        // true
+10 > 5 || 10 > 20;    // true
+false || false;       // false
+10 > 100 || 10 > 20;  // false
+
+//Ternary Operator
+
+function Membrecia()
+{
+    let Cliente = true;
+    let Cliente1 = false; 
+    
+    Cliente == true ? console.log("Permitir entrada") : console.log("NO puede ingresar");
+    Cliente1 == true ? console.log("Permitir entrada") : console.log("NO puede ingresar");
+}
+
+Membrecia();
+//Else Statement
+
+//Logical Operator &&
+
+//Switch Statement
+
+//If Statement
+
+//Logical Operator !
+
+//Comparison Operators
+
+//Else if Clause
+
+//Truthy and Falsy
