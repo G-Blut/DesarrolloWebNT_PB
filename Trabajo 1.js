0916f764bea3f2de986614876de9b1c3393ff16b
Jaiber Jiménez <jaiber.sevulpeda494@pascualbravo.edu.co>
2023-08-19 00:58:52 -0500

    Fueron añadidos las variables let y const junto con una funcion

---
+++ Trabajo 1.js
@@ -0,0 +1,20 @@
+let Person ="Peter"
+
+let Person1="Dana"
+
+const Superior = "Gerente"
+
+//console.log(Person,Person1)
+
+function Saludar_Personal()
+{
+    const Cordial = "Buenos dias";
+
+    console.log(Cordial, Person)
+
+    console.log(Cordial, Superior)
+
+    console.log(Cordial, Person1)
+}
+
+Saludar_Personal();
