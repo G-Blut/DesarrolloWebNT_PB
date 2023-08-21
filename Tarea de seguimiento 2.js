301ebe6da7b5ba2155cc22c036f253c87b4f1d52
Jaiber Jiménez <jaiber.sevulpeda494@pascualbravo.edu.co>
2023-08-21 02:16:40 -0500

    2 funciones en flecha y una 3ra que funciona como callback

---
+++ Tarea de seguimiento 2.js
@@ -0,0 +1,31 @@
+const especies = ['Gato', 'Caballo','Escorpion', 'Perro'];
+
+const PruebaCallback = () => {
+    
+    console.log('Callback llamado con exito.')
+}
+
+const Arreglo1 = (data,callback) =>
+{
+    data.pop()
+    callback()
+    return data
+}
+
+console.log(Arreglo1(especies,PruebaCallback))
+
+const especies1 = ['Gato', 'Caballo','Escorpion', 'Perro'];
+
+// const PruebaCallback = () => {
+    
+//     console.log('Callback llamado con exito.')
+// }
+
+const Arreglo2 = (data1,dato1,dato2,callback) => 
+{
+        data1.push(dato1,dato2)
+        callback()
+        console.log(data1)
+
+    }
+Arreglo2(especies1,'Kavat','Kubrow',PruebaCallback)
