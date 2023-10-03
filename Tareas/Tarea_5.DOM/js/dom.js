9c665677705fe57101a00b03d5258c73b28f679c
Jaiber Jiménez <jaiber.sevulpeda494@pascualbravo.edu.co>
2023-10-03 09:35:48 -0500

    Entrega DOM JS
    Creacion de los modulos para la entrega

---
+++ Tareas/Tarea_5.DOM/js/dom.js
@@ -0,0 +1,24 @@
+export const pruebainsercion = `
+    <small>Aqui esta tu resultado</small>
+    <br>
+    <strong>Espero que este programa haya sido de tu agrado.</strong>
+    `
+
+
+
+export const opcion=`
+    <p>Motivos por los cuales nos debes de elegir</p>
+    <ul>
+        <li>Este programa es superio a la competencia</li>
+        <li>El mejor resultado gratis</li>
+    </ul>
+    `
+
+
+export const prelude = `
+<section>
+<ul class="container">
+<img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" alt="Error al cargar imagen.jpg xd">
+</ul>
+</section>
+`
